229fb37850b5dd00e8a072099fe6b392b2a9dd5c
Наконечный Антон Викторович <anakonechny@alfabank.ru>
2022-12-16 03:04:17 +0300

    feat: add interactive stream

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+import { ask } from "./services/interactive-cli.js";
 import { getArgs } from "./utils/args.js";
 
 const { username } = getArgs();
 
 console.log(`Welcome to the File Manager, ${username}!`);
+
+process.on("SIGINT", () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    process.exit();
+});
+
+while (true) {
+    const answer = await ask();
+
+    if (answer === ".exit") {
+        process.emit("SIGINT");
+    }
+
+    console.log(answer);
+}
